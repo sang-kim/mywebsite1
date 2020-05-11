9d1de4e2beb4d8d7da41dcab0ac11adc0c1c35b4
cryptokn1ght <sangwonk645@gmail.com>
2020-05-11 12:41:39 -0400

    First commit Message

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){ 
+
+	let dark = localStorage.getItem('dark');
+
+	const darkToggle = document.querySelector('#checkbox');
+
+	const enableDark = () => {
+		// add darkmode class to the body
+		document.body.classList.add('dark');
+		// update darkmode in local storage
+		localStorage.setItem('dark','enabled')
+	};
+
+	const disableDark = () => {
+		// add darkmode class to the body
+		document.body.classList.remove('dark');
+		// update darkmode in local storage
+		localStorage.setItem('dark',null)
+	};
+
+	if (dark == 'enabled') {
+		enableDark();
+	};
+
+	darkToggle.addEventListener("click", () => {
+		dark = localStorage.getItem('dark');
+		if (dark !== 'enabled') {
+			enableDark();
+		} else {
+			disableDark();
+		}
+	});
+
+	function swapStyleSheet(sheet){
+	  document.getElementById('checkbox').setAttribute('href', sheet);
+	  localStorage.setItem("sheet", sheet);
+	}
+
+	// scrollspy //
+	window.onload = _ =>
+	 swapStyleSheet(
+	  localStorage.getItem("sheet") || "default.css"
+	 );
+
+	$(window).bind('scroll', function() {
+    var currentTop = $(window).scrollTop();
+    var elems = $('.scrollspy');
+    elems.each(function(index){
+      var elemTop 	= $(this).offset().top;
+      var elemBottom 	= elemTop + $(this).height();
+      if(currentTop >= elemTop && currentTop <= elemBottom){
+        var id 		= $(this).attr('id');
+        var navElem = $('a[href="#' + id+ '"]');
+    navElem.parent().addClass('active').siblings().removeClass( 'active' );
+      }
+    })
+});
+
+}); //document boiler plate end
